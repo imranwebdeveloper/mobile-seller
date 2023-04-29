5b3db9671ef647a7ee67036c791f9667c2ef7baf
Imran Nazir <imran.web.developer2021@gmail.com>
2023-04-29 13:34:21 +0600

    added category page and pagination

--- app/(mobile)/category/[slug]/page.tsx
+++ app/(mobile)/category/[slug]/page.tsx
@@ -1,10 +1,57 @@
+import MobileCardContainer from "@/components/common/MobileCardContainer";
+import Pagination from "@/components/common/Pagination";
+import PhoneCategory from "@/components/common/PhoneCategory";
+import PriceRange from "@/components/common/PriceRange";
+import SubHeader from "@/components/container/header/SubHeader";
 import React from "react";
 
-const CategoryPhones = async ({ params }: { params: { slug: string } }) => {
-  console.log(params.slug);
+const getData = async (slug: string, pageNumber?: string) => {
+  let url: string;
+  url = pageNumber
+    ? (`${process.env.API_URL}/mobiles/category/${slug}?page=${pageNumber}` as string)
+    : (`${process.env.API_URL}/mobiles/category/${slug}` as string);
+
+  const res = await fetch(url, { cache: "no-cache" });
+  return res.json();
+};
+
+const CategoryPhones = async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { page: string };
+}) => {
+  const {
+    data: { parPage, count, mobiles },
+  } = await getData(params.slug, searchParams.page);
+  const category = params.slug
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+  category.charAt(0).toUpperCase();
   return (
     <section className="main">
-      <section className="layout container"> Category </section>
+      <section className="layout container">
+        <SubHeader />
+        <PriceRange />
+        <PhoneCategory />
+        <div>
+          <div className="my-2 mt-4">
+            <h1 className="text-2xl">Latest {category} Price in Bangladesh</h1>
+          </div>
+          <MobileCardContainer data={mobiles} />
+        </div>
+        {mobiles.length >= 12 && (
+          <Pagination
+            currenPage={parseInt(searchParams.page)}
+            totalProduct={count}
+            parPage={parPage}
+            path={`category/${params.slug}`}
+          />
+        )}
+      </section>
     </section>
   );
 };
--- app/(mobile)/mobile/[brand]/page.tsx
+++ app/(mobile)/mobile/[brand]/page.tsx
@@ -1,17 +1,29 @@
 import MobileCardContainer from "@/components/common/MobileCardContainer";
+import Pagination from "@/components/common/Pagination";
 import SubHeader from "@/components/container/header/SubHeader";
-const getData = async (slug: string) => {
-  const res = await fetch(
-    `${process.env.API_URL}/mobiles/brand/${slug}` as string,
-    {
-      cache: "no-cache",
-    }
-  );
+
+const getData = async (slug: string, pageNumber?: string) => {
+  let url: string;
+  url = pageNumber
+    ? (`${process.env.API_URL}/mobiles/brand/${slug}?page=${pageNumber}` as string)
+    : (`${process.env.API_URL}/mobiles/brand/${slug}` as string);
+
+  const res = await fetch(url, { cache: "no-cache" });
   return res.json();
 };
 
-const BrandModelList = async ({ params }: { params: { brand: string } }) => {
-  const { data } = await getData(params.brand);
+const BrandModelList = async ({
+  params,
+  searchParams,
+}: {
+  params: { brand: string };
+  searchParams: { page: string };
+}) => {
+  const {
+    data: { parPage, count, mobiles },
+  } = await getData(params.brand, searchParams.page);
+  const currenPage = parseInt(searchParams.page);
+
   return (
     <div className="main">
       <section className="layout container">
@@ -19,7 +31,15 @@ const BrandModelList = async ({ params }: { params: { brand: string } }) => {
         <div className="mb-2 mt-4">
           <h1>{`${params.brand.toUpperCase()} MOBILES  `}</h1>
         </div>
-        <MobileCardContainer data={data} />
+        <MobileCardContainer data={mobiles} />
+        {mobiles.length >= parPage && (
+          <Pagination
+            path={`mobile/${params.brand}`}
+            parPage={parPage}
+            totalProduct={count}
+            currenPage={currenPage ? currenPage : 1}
+          />
+        )}
       </section>
     </div>
   );
