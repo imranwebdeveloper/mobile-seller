5131c37cfa16ab9a8dfcd1b9b5872b0e559096ad
Imran Nazir <imran.web.developer2021@gmail.com>
2023-05-02 15:11:16 +0600

    add price rang page

---
+++ app/(mobile)/price-range/[slug]/page.tsx
@@ -0,0 +1,54 @@
+import MobileCardContainer from "@/components/common/MobileCardContainer";
+import PhoneCategory from "@/components/common/PhoneCategory";
+import SubHeader from "@/components/container/header/SubHeader";
+import PriceRange from "@/components/common/PriceRange";
+
+import React from "react";
+import Pagination from "@/components/common/Pagination";
+
+const getData = async (slug: string, pageNumber?: string) => {
+  let url: string;
+  url = pageNumber
+    ? (`${process.env.API_URL}/mobiles/price/${slug}?page=${pageNumber}` as string)
+    : (`${process.env.API_URL}/mobiles/price/${slug}` as string);
+
+  const res = await fetch(url, { cache: "no-cache" });
+  return res.json();
+};
+
+const PriceRanges = async ({
+  searchParams,
+  params,
+}: {
+  searchParams: { page: string };
+  params: { slug: string };
+}) => {
+  const {
+    data: { parPage, count, mobiles },
+  } = await getData(params.slug, searchParams.page);
+  return (
+    <section className="main">
+      <section className="layout container">
+        <SubHeader />
+        <PriceRange />
+        <PhoneCategory />
+        <div>
+          <div className="my-2 mt-4">
+            <h1 className="text-2xl">Latest Mobiles Price in Bangladesh</h1>
+          </div>
+          <MobileCardContainer data={mobiles} />
+        </div>
+        {mobiles.length >= 12 && (
+          <Pagination
+            currenPage={parseInt(searchParams.page ? searchParams.page : "1")}
+            totalProduct={count}
+            parPage={parPage}
+            path={`price-range/${params.slug}`}
+          />
+        )}
+      </section>
+    </section>
+  );
+};
+
+export default PriceRanges;
--- components/common/PriceRange.tsx
+++ components/common/PriceRange.tsx
@@ -8,22 +8,22 @@ const PriceRange = () => {
       <p className="font-bold">Price Range :</p>
       <ul className="flex justify-start whitespace-nowrap gap-1  text-sm md:text-base divide-x rounded  overflow-x-scroll scrollbar-hide ">
         <li className="px-2 md:px-4 py-1 border rounded-full bg-primary-bg-light ">
-          <Link href="/category/tablets">Tk 1 - 5000</Link>
+          <Link href="/price-range/1-5000">Tk 1 - 5000</Link>
         </li>
         <li className="px-2 md:px-4 py-1 border rounded-full bg-primary-bg-light ">
-          <Link href="/category/smartphones">Tk 5001 - 10000</Link>
+          <Link href="/price-range/5001-10000">Tk 5001 - 10000</Link>
         </li>
         <li className="px-2 md:px-4 py-1 border rounded-full bg-primary-bg-light ">
-          <Link href="/category/smartphones">Tk 10001 - 20000</Link>
+          <Link href="/price-range/10001-20000">Tk 10001 - 20000</Link>
         </li>
         <li className="px-2 md:px-4 py-1 border rounded-full bg-primary-bg-light ">
-          <Link href="/category/smartphones">Tk 20001 - 30000</Link>
+          <Link href="/price-range/20001-30000">Tk 20001 - 30000</Link>
         </li>
         <li className="px-2 md:px-4 py-1 border rounded-full bg-primary-bg-light ">
-          <Link href="/category/smartphones">Tk 30001 - 50000</Link>
+          <Link href="/price-range/30001-50000">Tk 30001 - 50000</Link>
         </li>
         <li className="px-2 md:px-4 py-1 border rounded-full bg-primary-bg-light ">
-          <Link href="/category/smartphones">Tk 50000+</Link>
+          <Link href="/price-range/50001">Tk 50001+</Link>
         </li>
       </ul>
     </div>
