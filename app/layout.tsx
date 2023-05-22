9cbb4959e1dd3797959683f7fac92c0f5976b398
Imran Nazir <imran.web.developer2021@gmail.com>
2023-05-22 11:07:05 +0600

    added tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,18 +61,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={lato.className}>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-W7NHE0JT3Z"
-      />
-      <Script id="google-analytics">
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  
-  gtag('config', 'G-W7NHE0JT3Z');`}
+      {/* Google Tag Manager  */}
+
+      <Script id="google-tag-manager">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5HSV35W');`}
       </Script>
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* Google Tag Manager (noscript)  */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5HSV35W"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+      </body>
     </html>
   );
 }
