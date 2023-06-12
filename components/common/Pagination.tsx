4aa35b6db18bd88fbfd41efff28753de4bffaea5
Imran Nazir <imran.web.developer2021@gmail.com>
2023-06-12 11:34:28 +0600

    pagination bug fixed

--- components/common/Pagination.tsx
+++ components/common/Pagination.tsx
@@ -17,28 +17,34 @@ const Pagination: React.FC<Props> = ({
 }: Props) => {
   const totalPage = Math.ceil(totalProduct / parPage);
   const numOfPages = Array.from({ length: totalPage }, (_, i) => i + 1);
+
+  const shortPagination = currenPage <= 1 ? 0 : currenPage - 2;
+  const pagination = numOfPages.slice(shortPagination, shortPagination + 4);
   return (
-    <nav className="flex justify-center my-4">
-      <ul className="gap-3 flex bg-primary-bg-light px-4  py-3 items-center  rounded-md">
-        <li>
-          <Link
-            href={`${
-              currenPage <= 1
-                ? `${path}?page=${1}`
-                : `${path}?page=${currenPage - 1}`
-            }`}
-            className="text-link-text-light font-bold px-2"
-          >
-            Previous
-          </Link>
-        </li>
-        {numOfPages.map((item) => {
+    <nav className="flex justify-center my-4 text-sm md:text-base">
+      <ul className="gap-3 flex bg-primary-bg-light border px-4  py-3 items-center  rounded-md">
+        {currenPage >= 2 && (
+          <li>
+            <Link
+              href={`${
+                currenPage <= 1
+                  ? `${path}?page=${1}`
+                  : `${path}?page=${currenPage - 1}`
+              }`}
+              className=" text-origin-600 font-bold px-2"
+            >
+              Previous
+            </Link>
+          </li>
+        )}
+
+        {pagination.map((item) => {
           return (
             <li key={item}>
               <Link
                 className={` ${
                   currenPage === item &&
-                  "bg-slate-200 font-bold text-link-text-light"
+                  "bg-origin-50 font-bold text-origin-600"
                 }  rounded px-3 py-1 `}
                 href={`${path}?page=${item}`}
               >
@@ -50,7 +56,7 @@ const Pagination: React.FC<Props> = ({
         <li>
           <Link
             href={`${path}?page=${currenPage + 1}`}
-            className="text-link-text-light font-bold px-2"
+            className="text-origin-600 font-bold px-2"
           >
             Next
           </Link>
